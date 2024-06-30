fbdc428a4365e09ef258f13722f2b845b1439920
emmanuelle <emmanuelledennemont@orange.fr>
2024-06-30 08:52:32 +0200

    Initial Commit

---
+++ AnimatedScreen/AnimatedScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedScreenApp.swift
+//  AnimatedScreen
+//
+//  Created by Emmanuelle  Dennemont on 30/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
